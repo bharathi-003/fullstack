13e030823aa227947357010945028d5de46e15c2
bharathi-003 <bharathiananth@gmail.com>
2026-02-05 15:04:03 +0530

    push

--- src/brouse.jsx
+++ src/brouse.jsx
@@ -1,6 +1,6 @@
-
+import style from './Brouse.css'
 function Brouse(){
-    return(<button className="button">click me</button>);
+    return(<button className={style.button}>click me</button>);
 
 }
 export default Brouse;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Brouse from './Brouse.jsx'
-import Menu from './Day1/Menu.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Menu/>
     <App />
     <Brouse/>
   </StrictMode>,
